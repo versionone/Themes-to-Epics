970f3041eb5cc17b6028f338b4287b33b8b2b23e
rajiv <rajiv.delwadia@versionone.com>
2012-09-11 17:37:41 -0400

    clean up assembly info

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Themes-to-Epics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Themes-to-Epics")]
+[assembly: AssemblyCopyright("Copyright ©  2012 VersionOne, Inc")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1-a")]
