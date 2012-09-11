de2bedbd9fd922ac5c65b8fdb6cbd55238499595
rajiv <rajiv.delwadia@versionone.com>
2012-09-11 17:45:00 -0400

    clean up assets created by unit tests

--- Tests/WhenGeneratingAnEpicFromATheme.cs
+++ Tests/WhenGeneratingAnEpicFromATheme.cs
@@ -6,30 +6,36 @@ namespace VersionOne.Themes_to_Epics.Tests
 	[TestFixture]
 	public class WhenGeneratingAnEpicFromATheme : WithV1Instance
 	{
+		private Project _project;
 		private Theme _theme;
 		private Epic _epic;
 
 		[TestFixtureSetUp]
-		public void Setup()
+		public void SetUp()
 		{
-			GivenATheme();
-			WhenGeneratingAnEpic();
-		}
-
-		private void GivenATheme()
-		{
-			_theme = V1.Create.Theme(Random.Name(), NewProject());
-		}
-
-		private void WhenGeneratingAnEpic()
-		{
+			_project = NewProject();
+			_theme = V1.Create.Theme(Random.Name(), _project);
 			_epic = Program.GenerateEpicFrom(_theme, V1);
+		}
+
+		[TestFixtureTearDown]
+		public void TearDown()
+		{
+			if (_epic != null) _epic.Delete();
+			if (_theme != null) _theme.Delete();
+			if (_project != null) _project.Delete();
 		}
 
 		[Test]
 		public void TheEpicShouldHaveTheSameName()
 		{
 			Assert.That(_epic.Name, Is.EqualTo(_theme.Name));
+		}
+
+		[Test]
+		public void TheEpicShouldHaveTheSameProject()
+		{
+			Assert.That(_epic.Project, Is.EqualTo(_theme.Project));
 		}
 	}
 }
