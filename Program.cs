eed117ed0580aa8d79d31fe96fe8db013678d79a
rajiv <rajiv.delwadia@versionone.com>
2012-09-14 10:44:15 -0400

    refactor massive Run method

--- Program.cs
+++ Program.cs
@@ -34,20 +34,41 @@ namespace VersionOne.Themes_to_Epics
 
 		private static void Run(Options options)
 		{
-			Console.WriteLine("Connecting to {0} ...", options.Url);
-			V1Instance v1Instance = new V1Instance(options.Url, options.Username, options.Password);
-			V1Adapter v1Adapter = new V1Adapter(v1Instance);
-			Console.WriteLine("Connected.");
+			var v1Instance = Connect(options);
 
-			Console.WriteLine("Resolving project {0} ...", options.Scope);
-			Project project = new ProjectResolver(v1Instance).Resolve(options.Scope);
-			Console.WriteLine("Resolved to \"{0}\" ({1}).", project.Name, project.ID);
+			var project = Resolve(options.Scope, v1Instance);
 
-			EpicGenerator generator = new EpicGenerator(project, v1Adapter);
-
-			Console.WriteLine("Processing themes...");
+			var generator = new EpicGenerator(project, new V1Adapter(v1Instance));
 
 			IDictionary<Theme, Epic> map = new Dictionary<Theme, Epic>();
+
+			Convert(generator, map);
+
+			Tree(map);
+
+			Reassign(generator, map);
+		}
+
+		private static V1Instance Connect(Options options)
+		{
+			Console.WriteLine("Connecting to {0} ...", options.Url);
+			V1Instance v1Instance = new V1Instance(options.Url, options.Username, options.Password);
+			Console.WriteLine("Connected.");
+			return v1Instance;
+		}
+
+		private static Project Resolve(string moniker, V1Instance v1Instance)
+		{
+			Console.WriteLine("Resolving {0} ...", moniker);
+			Project project = new ProjectResolver(v1Instance).Resolve(moniker);
+			Console.WriteLine("Resolved to {0}.", Reference(project));
+			return project;
+		}
+
+		private static void Convert(EpicGenerator generator, IDictionary<Theme, Epic> map)
+		{
+			Console.WriteLine("Processing themes...");
+
 			foreach (var theme in generator.ChooseThemes())
 			{
 				Console.Write("\t{0} -> ", Reference(theme));
@@ -57,7 +78,10 @@ namespace VersionOne.Themes_to_Epics
 			}
 
 			Console.WriteLine("{0} themes processed.", map.Count);
+		}
 
+		private static void Tree(IDictionary<Theme, Epic> map)
+		{
 			Console.WriteLine("Parenting generated epics...");
 			var count = 0;
 
@@ -80,9 +104,12 @@ namespace VersionOne.Themes_to_Epics
 			}
 
 			Console.WriteLine("{0} generated epics parented.", count);
+		}
 
+		private static void Reassign(EpicGenerator generator, IDictionary<Theme, Epic> map)
+		{
 			Console.WriteLine("Assigning existing epics to generated epics");
-			count = 0;
+			var count = 0;
 
 			foreach (Epic epic in generator.ChooseEpics())
 			{
@@ -98,6 +125,11 @@ namespace VersionOne.Themes_to_Epics
 			}
 
 			Console.WriteLine("{0} existing epics assigned to generated epics.", count);
+		}
+
+		private static string Reference(BaseAsset asset)
+		{
+			return string.Format("\"{0}\" ({1})", asset.Name, asset.ID);
 		}
 
 		private static string Reference(ProjectAsset asset)
